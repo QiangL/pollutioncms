7bb1de87b1eb553143a4bfdadfe5f5b04657f4c2
QiangL <iamlq@live.cn>
2018-04-09 22:10:25 +0800

    准备合并另一部分

---
+++ src/main/java/com/pollutioncms/auth/TestRealm.java
@@ -0,0 +1,58 @@
+package com.pollutioncms.auth;
+
+import com.pollutioncms.module.domain.Role;
+import com.pollutioncms.module.domain.User;
+import org.apache.shiro.authc.*;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.authz.SimpleAuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class TestRealm extends AuthorizingRealm {
+
+    /**
+     * 权限认证
+     */
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
+        //获取登录时输入的用户名
+        /*String loginName=(String) principalCollection.fromRealm(getName()).iterator().next();
+        //到数据库查是否有此对象
+        User user=userService.findByName(loginName);
+        if(user!=null){
+            //权限信息对象info,用来存放查出的用户的所有的角色（role）及权限（permission）
+            SimpleAuthorizationInfo info=new SimpleAuthorizationInfo();
+            //用户的角色集合
+            info.setRoles(user.getRolesName());
+            //用户的角色对应的所有权限，如果只使用角色定义访问权限，下面的四行可以不要
+            List<Role> roleList=user.getRoleList();
+            for (Role role : roleList) {
+                info.addStringPermissions(role.getPermissionsName());
+            }
+            return info;
+        }*/
+        return null;
+    }
+
+    /**
+     * 登录认证;
+     */
+    protected AuthenticationInfo doGetAuthenticationInfo(
+            AuthenticationToken authenticationToken) throws AuthenticationException {
+        //UsernamePasswordToken对象用来存放提交的登录信息
+        /*UsernamePasswordToken token=(UsernamePasswordToken) authenticationToken;
+        //查出是否有此用户
+        User user=userService.findByName(token.getUsername());
+        if(user!=null){
+            //若存在，将此用户存放到登录认证info中
+            return new SimpleAuthenticationInfo(user.getUsername(), user.getPassword(), getName());
+        }*/
+        return null;
+    }
+
+}
