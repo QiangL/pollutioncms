7f70fc7da1c0eb0e839dd0e078124731edbf3361
QiangL <iamlq@live.cn>
2018-04-12 20:19:03 +0800

    改正usermapper映射文件错误

--- src/main/java/com/pollutioncms/web/controller/TestController.java
+++ src/main/java/com/pollutioncms/web/controller/TestController.java
@@ -1,17 +1,24 @@
 package com.pollutioncms.web.controller;
 
+import com.pollutioncms.module.mapper.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Set;
+
 @Controller
 public class TestController {
 
+    @Autowired
+    private UserMapper userMapper;
+
     @RequestMapping(value="/test.mvc",method = RequestMethod.GET)
     @ResponseBody
-    public String test(){
-        return "test";
+    public Set<String> test(){
+        return userMapper.listPermissions(1);
     }
 }
