354020ded51cd44a428c96fe80dc643f1d89b435
QiangL <iamlq@live.cn>
2018-04-12 18:59:57 +0800

    使用通用mapper

--- src/main/java/com/pollutioncms/module/mapper/UserMapper.java
+++ src/main/java/com/pollutioncms/module/mapper/UserMapper.java
@@ -1,7 +1,12 @@
 package com.pollutioncms.module.mapper;
 
 import com.pollutioncms.module.domain.User;
+import org.apache.ibatis.annotations.Param;
 import tk.mybatis.mapper.common.Mapper;
 
+import java.util.Set;
+
 public interface UserMapper extends Mapper<User> {
+
+    Set<String> listPermissions(@Param("id") Integer id);
 }
