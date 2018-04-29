d70ebb42c7c99844465102903c71b0c5a6386231
QiangL <iamlq@live.cn>
2018-04-29 15:11:22 +0800

    修改sql文件

--- src/main/resources/sql/hiwater.sql
+++ src/main/resources/sql/hiwater.sql
@@ -16,28 +16,30 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `first_module`
+-- Table structure for table `module`
 --
 
-DROP TABLE IF EXISTS `first_module`;
+DROP TABLE IF EXISTS `module`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `first_module` (
-  `uuid` char(36) NOT NULL,
+CREATE TABLE `module` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `parent_id` int(11) NOT NULL DEFAULT '0',
   `name` varchar(50) NOT NULL,
-  PRIMARY KEY (`uuid`),
-  UNIQUE KEY `uuid_UNIQUE` (`uuid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uuid_UNIQUE` (`id`),
+  KEY `uuid_fk_idx` (`parent_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `first_module`
+-- Dumping data for table `module`
 --
 
-LOCK TABLES `first_module` WRITE;
-/*!40000 ALTER TABLE `first_module` DISABLE KEYS */;
-INSERT INTO `first_module` VALUES ('22d6ca44-3ee7-11e8-8d99-a01d48d62fec','测试模块');
-/*!40000 ALTER TABLE `first_module` ENABLE KEYS */;
+LOCK TABLES `module` WRITE;
+/*!40000 ALTER TABLE `module` DISABLE KEYS */;
+INSERT INTO `module` VALUES (1,0,'第一模块'),(4,0,'第二模块'),(5,0,'第三模块'),(6,0,'第四'),(7,4,'第五模块'),(8,4,'第六模块'),(9,5,'第七模块'),(10,9,'第好几个');
+/*!40000 ALTER TABLE `module` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -48,17 +50,16 @@ DROP TABLE IF EXISTS `module_operate`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `module_operate` (
-  `uuid` char(36) NOT NULL,
-  `parent_uuid` char(36) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `parent_id` int(11) NOT NULL,
   `name` varchar(50) NOT NULL,
-  `url` varchar(500) NOT NULL,
   `permission` varchar(45) NOT NULL,
-  PRIMARY KEY (`uuid`),
-  UNIQUE KEY `uuid_UNIQUE` (`uuid`),
-  KEY `uuid_fk_idx` (`parent_uuid`),
-  KEY `uuid_permission` (`uuid`,`permission`),
-  CONSTRAINT `second_module_uuid_fk` FOREIGN KEY (`parent_uuid`) REFERENCES module (id) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `comment` varchar(100) DEFAULT NULL COMMENT '备注',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uuid_UNIQUE` (`id`),
+  KEY `uuid_fk_idx` (`parent_id`),
+  KEY `uuid_permission` (`id`,`permission`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -67,7 +68,7 @@ CREATE TABLE `module_operate` (
 
 LOCK TABLES `module_operate` WRITE;
 /*!40000 ALTER TABLE `module_operate` DISABLE KEYS */;
-INSERT INTO `module_operate` VALUES ('55f6b141-3ee7-11e8-8d99-a01d48d62fed','55f6b141-3ee7-11e8-8d99-a01d48d62fec','测试模块操作','/test','test');
+INSERT INTO `module_operate` VALUES (1,7,'用户','user','用户信息'),(2,7,'角色','role',NULL),(3,8,'模块','module',NULL);
 /*!40000 ALTER TABLE `module_operate` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -107,12 +108,14 @@ DROP TABLE IF EXISTS `role`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `role` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `role_cn_name` varchar(45) DEFAULT NULL COMMENT '备注',
+  `parent_id` int(11) NOT NULL COMMENT '创造Role的User_id',
   `role_name` varchar(50) DEFAULT NULL,
+  `role_cn_name` varchar(45) DEFAULT NULL COMMENT '备注',
   `comment` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `id_UNIQUE` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+  UNIQUE KEY `id_UNIQUE` (`id`),
+  UNIQUE KEY `role_name_index` (`role_name`)
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -121,7 +124,7 @@ CREATE TABLE `role` (
 
 LOCK TABLES `role` WRITE;
 /*!40000 ALTER TABLE `role` DISABLE KEYS */;
-INSERT INTO `role` VALUES (1,'超级管理员','admin',NULL),(2,'陌生人','guest',NULL);
+INSERT INTO `role` VALUES (1,0,'admin','超级管理员','test'),(2,0,'guest','陌生','');
 /*!40000 ALTER TABLE `role` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -133,15 +136,16 @@ DROP TABLE IF EXISTS `role_auth`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `role_auth` (
-  `id` char(36) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `role_id` int(11) NOT NULL,
-  `module_op_uuid` char(36) NOT NULL,
+  `module_op_id` int(11) NOT NULL,
+  `can_inherit` tinyint(1) DEFAULT '0' COMMENT '能否传递，给别人授权',
   PRIMARY KEY (`id`),
   KEY `role_fk_idx` (`role_id`),
-  KEY `module_op_uuid_fk_idx` (`module_op_uuid`),
-  CONSTRAINT `module_op_uuid_fk` FOREIGN KEY (`module_op_uuid`) REFERENCES `module_operate` (`uuid`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  KEY `module_op_uuid_fk_idx` (`module_op_id`),
+  CONSTRAINT `module_ops_fk` FOREIGN KEY (`module_op_id`) REFERENCES `module_operate` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `role_fk` FOREIGN KEY (`role_id`) REFERENCES `role` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -150,7 +154,7 @@ CREATE TABLE `role_auth` (
 
 LOCK TABLES `role_auth` WRITE;
 /*!40000 ALTER TABLE `role_auth` DISABLE KEYS */;
-INSERT INTO `role_auth` VALUES ('1',1,'55f6b141-3ee7-11e8-8d99-a01d48d62fed');
+INSERT INTO `role_auth` VALUES (1,1,1,0),(6,1,2,0),(7,1,3,0);
 /*!40000 ALTER TABLE `role_auth` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -171,7 +175,7 @@ CREATE TABLE `role_user` (
   KEY `user_id_fk_idx` (`user_id`),
   CONSTRAINT `role_id_fk1` FOREIGN KEY (`role_id`) REFERENCES `role` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `user_id_fk` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=60 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -180,36 +184,8 @@ CREATE TABLE `role_user` (
 
 LOCK TABLES `role_user` WRITE;
 /*!40000 ALTER TABLE `role_user` DISABLE KEYS */;
-INSERT INTO `role_user` VALUES (1,1,1),(2,2,1);
+INSERT INTO `role_user` VALUES (33,1,1),(34,2,1),(37,1,1);
 /*!40000 ALTER TABLE `role_user` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `second_module`
---
-
-DROP TABLE IF EXISTS module;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `second_module` (
-  `uuid` char(36) NOT NULL,
-  `parent_uuid` char(36) NOT NULL,
-  `name` varchar(50) NOT NULL,
-  PRIMARY KEY (`uuid`),
-  UNIQUE KEY `uuid_UNIQUE` (`uuid`),
-  KEY `uuid_fk_idx` (`parent_uuid`),
-  CONSTRAINT `first_module_uuid_fk` FOREIGN KEY (`parent_uuid`) REFERENCES `first_module` (`uuid`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `second_module`
---
-
-LOCK TABLES module WRITE;
-/*!40000 ALTER TABLE module DISABLE KEYS */;
-INSERT INTO module VALUES ('55f6b141-3ee7-11e8-8d99-a01d48d62fec','22d6ca44-3ee7-11e8-8d99-a01d48d62fec','测试第二模块');
-/*!40000 ALTER TABLE module ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -221,19 +197,22 @@ DROP TABLE IF EXISTS `user`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `user_name` varchar(10) NOT NULL COMMENT '登录名',
-  `pwd` varchar(45) NOT NULL,
+  `user_name` varchar(50) NOT NULL COMMENT '登录名',
+  `pwd` varchar(128) NOT NULL,
   `create_time` datetime NOT NULL,
   `show_name` varchar(45) DEFAULT NULL COMMENT '显示名称，展示用',
   `user_sex` int(11) DEFAULT NULL,
   `user_tel` char(11) DEFAULT NULL,
   `user_addr` varchar(100) DEFAULT NULL,
   `user_email` varchar(45) DEFAULT NULL,
-  `last_opt_time` datetime NOT NULL COMMENT '最后一次操作事件',
+  `last_opt_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '最后一次操作事件',
+  `salt` varchar(100) DEFAULT '' COMMENT '密码加盐',
+  `status` int(11) DEFAULT '1' COMMENT '用户状态\n1：正常\n2：锁定，不能登录\n3：删除',
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_UNIQUE` (`id`),
-  UNIQUE KEY `id_name_index` (`id`,`user_name`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+  UNIQUE KEY `id_name_index` (`id`,`user_name`),
+  UNIQUE KEY `name_UNIQUE` (`user_name`)
+) ENGINE=InnoDB AUTO_INCREMENT=58 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -242,7 +221,7 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (1,'liqiang','liqiang','2018-01-01 00:00:00','李强',1,NULL,NULL,NULL,'2018-04-12 14:09:32');
+INSERT INTO `user` VALUES (1,'liqiang','f4bc8408a07e3ccd3be710f47411bcef69d02d42b9118b72c235888f8d30e38a084755acc4ae15f58db5e79f87755e12bf2f12ae8392788ff12da4b6cb28cb7e','2018-01-01 00:00:00','李强',1,'18340861214','aaa','1214098151@qq.com','2018-04-12 14:09:32','222059888578feb262b1d398f4ba3bc6',1),(57,'201292162.333','909d975be0d6a1ad3550eafb69730299c1bec8bdeaf14b36b00c93dff708896c37e4134ede0416ffd1be207c4ef222e6037d28512478d75fc086885eeab4dd1e','2018-04-29 14:35:59','test',1,'18340861214','test','1214098151@qq.com','2018-04-29 14:35:59','a08e47843d864d9e8014914b0dddb2a5',1);
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -255,4 +234,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-04-16 16:02:41
+-- Dump completed on 2018-04-29 15:10:48
