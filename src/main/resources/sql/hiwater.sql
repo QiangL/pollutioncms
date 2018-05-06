5a0b6b06f49ea4d3d07fa65359ef6e161d8e6ae8
QiangL <iamlq@live.cn>
2018-05-06 17:42:59 +0800

    update sql

--- src/main/resources/sql/hiwater.sql
+++ src/main/resources/sql/hiwater.sql
@@ -35,7 +35,7 @@ CREATE TABLE `module` (
   UNIQUE KEY `id_UNIQUE` (`id`),
   KEY `uuid_fk_idx` (`parent_id`),
   KEY `permission_idx` (`permission`)
-) ENGINE=InnoDB AUTO_INCREMENT=112 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=129 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -44,7 +44,7 @@ CREATE TABLE `module` (
 
 LOCK TABLES `module` WRITE;
 /*!40000 ALTER TABLE `module` DISABLE KEYS */;
-INSERT INTO `module` VALUES (11,107,'用户','user','',1,1,NULL),(12,106,'角色','role','',1,1,NULL),(13,104,'模块','module','',1,1,NULL),(14,105,'导航','navigation','',1,1,NULL),(19,0,'废水污染源自动监控',NULL,NULL,0,0,NULL),(20,0,'污染源信息管理',NULL,NULL,0,0,NULL),(21,0,'污染源自动采样',NULL,NULL,0,0,NULL),(22,0,'系统管理',NULL,NULL,0,0,NULL),(23,19,'实时监测',NULL,NULL,0,0,NULL),(24,19,'报警管理',NULL,NULL,0,0,NULL),(25,19,'审核管理',NULL,NULL,0,0,NULL),(26,19,'综合管理',NULL,NULL,0,0,NULL),(27,19,'报表统计',NULL,NULL,0,0,NULL),(28,19,'邮件服务',NULL,NULL,0,0,NULL),(29,19,'上报管理',NULL,NULL,0,0,NULL),(31,24,'报警日志',NULL,'/browseAlarmlogging.action',0,0,NULL),(32,24,'报警规则',NULL,'/browseAlarmRegular.action',0,0,NULL),(33,24,'短信设置',NULL,'/navigation_html/First/2-Alarm/MessageNotice.html',0,0,NULL),(34,24,'报警指令',NULL,'/navigation_html/First/2-Alarm/AlarmInstruct.html',0,0,NULL),(35,25,'审核数据',NULL,'/navigation_html/First/3-Audit/AuditData.html',0,0,NULL),(36,25,'审核规则',NULL,'/navigation_html/First/3-Audit/AuditRule.html',0,0,NULL),(37,25,'审核日志',NULL,'/navigation_html/First/3-Audit/AuditLog.html',0,0,NULL),(38,25,'日志生成',NULL,'/navigation_html/First/3-Audit/DataGeneration.html',0,0,NULL),(39,25,'审核结果',NULL,'/navigation_html/First/3-Audit/AuditResult.html',0,0,NULL),(40,26,'监控数据查询',NULL,'/navigation_html/First/4-SynthesizeQuery/MonitorDataQuery.html',0,0,NULL),(41,26,'报表数据查询',NULL,'/navigation_html/First/4-SynthesizeQuery/StatementDataQuery.html',0,0,NULL),(42,27,'排放统计月报',NULL,NULL,0,0,NULL),(43,27,'企业排放情况统计',NULL,NULL,0,0,NULL),(44,27,'污水处理厂情况统计',NULL,NULL,0,0,NULL),(45,27,'企业污染物排放统计',NULL,NULL,0,0,NULL),(46,27,'污水厂污染物排放统计',NULL,NULL,0,0,NULL),(47,27,'企业废水排放统计',NULL,NULL,0,0,NULL),(48,27,'企业超标统计（瞬时值）',NULL,NULL,0,0,NULL),(49,27,'企业超标统计（日均值）',NULL,NULL,0,0,NULL),(50,27,'污水厂超标统计（瞬时值）',NULL,NULL,0,0,NULL),(51,27,'污水厂超标统计（日均值）',NULL,NULL,0,0,NULL),(52,27,'在线监测数据审核报告',NULL,NULL,0,0,NULL),(53,27,'其他',NULL,NULL,0,0,NULL),(62,28,'发信箱',NULL,'/browseMail.action',0,0,NULL),(63,28,'写邮件',NULL,'/browseWriteMail.action',0,0,NULL),(64,28,'通讯簿',NULL,'/browseContacts.action',0,0,NULL),(65,28,'邮件设置',NULL,'/setMail.action',0,0,NULL),(66,29,'上报查询',NULL,NULL,0,0,NULL),(67,29,'上报设置',NULL,NULL,0,0,NULL),(68,29,'上报日志',NULL,NULL,0,0,NULL),(69,53,'排放统计月报-小时',NULL,NULL,0,0,NULL),(70,19,'运行维护',NULL,NULL,0,0,NULL),(71,70,'运维统计',NULL,NULL,0,0,NULL),(72,70,'运维配置',NULL,NULL,0,0,NULL),(73,71,'在线率概况',NULL,NULL,0,0,NULL),(74,71,'测点在线率详情',NULL,NULL,0,0,NULL),(75,71,'数据采样统计',NULL,NULL,0,0,NULL),(76,72,'维护组配置',NULL,NULL,0,0,NULL),(77,72,'维护类型配置',NULL,NULL,0,0,NULL),(78,72,'运行日历设置',NULL,NULL,0,0,NULL),(79,72,'仪器运行率标准',NULL,NULL,0,0,NULL),(80,20,'企业信息管理',NULL,'/browseEnt_view.action',0,0,NULL),(81,20,'排水口管理',NULL,'/browseOutfall_view.action',0,0,NULL),(82,20,'进水口管理',NULL,'/browseInlet.action',0,0,NULL),(83,20,'数采仪管理',NULL,'/browseSCY_view.action',0,0,NULL),(84,20,'进排口设置',NULL,'/navigation_html/Second/O&I_Set.html',0,0,NULL),(85,21,'在线列表',NULL,'/navigation_html/First/1-RealTimeMonitoring/OnlineTable.jsp',0,0,NULL),(86,21,'采样控制',NULL,NULL,0,0,NULL),(87,21,'报警管理',NULL,NULL,0,0,NULL),(88,21,'分析统计',NULL,NULL,0,0,NULL),(89,21,'分析统计',NULL,NULL,0,0,NULL),(90,86,'远程控制',NULL,NULL,0,0,NULL),(91,86,'采样计划',NULL,NULL,0,0,NULL),(92,86,'超标采样标准',NULL,NULL,0,0,NULL),(93,86,'采样任务查询',NULL,NULL,0,0,NULL),(94,86,'采样任务下达',NULL,NULL,0,0,NULL),(95,87,'报警日志',NULL,NULL,0,0,NULL),(96,87,'门禁管理',NULL,NULL,0,0,NULL),(97,88,'月报',NULL,NULL,0,0,NULL),(98,88,'季报',NULL,NULL,0,0,NULL),(99,88,'自定义报表',NULL,NULL,0,0,NULL),(100,88,'采样数据',NULL,NULL,0,0,NULL),(102,22,'框架管理',NULL,NULL,0,0,NULL),(103,22,'权限管理',NULL,NULL,0,0,NULL),(104,102,'模块管理','module:list','/systemManage/frameworkManage/moduleManage.jsp',1,0,NULL),(105,102,'导航管理','navigation:list','/systemManage/frameworkManage/navManage.jsp',1,0,NULL),(106,103,'角色管理','role:list','/systemManage/authManage/roleManage.jsp',1,0,NULL),(107,103,'用户管理','user:list','/systemManage/authManage/userManage.jsp',1,0,NULL),(108,22,'运行管理','',NULL,0,0,NULL),(109,108,'在线用户','onlineUser:list','/systemManage/runManage/onlineUser.jsp',1,0,NULL),(110,109,'下线用户','onlineUser:stop',NULL,1,1,NULL),(111,109,'列出在线用户','onlineUser:list',NULL,1,1,NULL);
+INSERT INTO `module` VALUES (11,103,'用户','user','',1,1,NULL),(12,103,'角色','role','',1,1,NULL),(19,0,'废水污染源自动监控',NULL,NULL,0,0,NULL),(20,0,'污染源信息管理',NULL,NULL,0,0,NULL),(21,0,'污染源自动采样',NULL,NULL,0,0,NULL),(22,0,'系统管理',NULL,NULL,0,0,NULL),(23,19,'实时监测',NULL,NULL,0,0,NULL),(24,19,'报警管理',NULL,NULL,0,0,NULL),(25,19,'审核管理',NULL,NULL,0,0,NULL),(26,19,'综合管理',NULL,NULL,0,0,NULL),(27,19,'报表统计',NULL,NULL,0,0,NULL),(28,19,'邮件服务',NULL,NULL,0,0,NULL),(29,19,'上报管理',NULL,NULL,0,0,NULL),(31,24,'报警日志',NULL,'/browseAlarmlogging.action',0,0,NULL),(32,24,'报警规则',NULL,'/browseAlarmRegular.action',0,0,NULL),(33,24,'短信设置',NULL,'/navigation_html/First/2-Alarm/MessageNotice.html',0,0,NULL),(34,24,'报警指令',NULL,'/navigation_html/First/2-Alarm/AlarmInstruct.html',0,0,NULL),(35,25,'审核数据',NULL,'/navigation_html/First/3-Audit/AuditData.html',0,0,NULL),(36,25,'审核规则',NULL,'/navigation_html/First/3-Audit/AuditRule.html',0,0,NULL),(37,25,'审核日志',NULL,'/navigation_html/First/3-Audit/AuditLog.html',0,0,NULL),(38,25,'日志生成',NULL,'/navigation_html/First/3-Audit/DataGeneration.html',0,0,NULL),(39,25,'审核结果',NULL,'/navigation_html/First/3-Audit/AuditResult.html',0,0,NULL),(40,26,'监控数据查询',NULL,'/navigation_html/First/4-SynthesizeQuery/MonitorDataQuery.html',0,0,NULL),(41,26,'报表数据查询',NULL,'/navigation_html/First/4-SynthesizeQuery/StatementDataQuery.html',0,0,NULL),(42,27,'排放统计月报',NULL,NULL,0,0,NULL),(43,27,'企业排放情况统计',NULL,NULL,0,0,NULL),(44,27,'污水处理厂情况统计',NULL,NULL,0,0,NULL),(45,27,'企业污染物排放统计',NULL,NULL,0,0,NULL),(46,27,'污水厂污染物排放统计',NULL,NULL,0,0,NULL),(47,27,'企业废水排放统计',NULL,NULL,0,0,NULL),(48,27,'企业超标统计（瞬时值）',NULL,NULL,0,0,NULL),(49,27,'企业超标统计（日均值）',NULL,NULL,0,0,NULL),(50,27,'污水厂超标统计（瞬时值）',NULL,NULL,0,0,NULL),(51,27,'污水厂超标统计（日均值）',NULL,NULL,0,0,NULL),(52,27,'在线监测数据审核报告',NULL,NULL,0,0,NULL),(53,27,'其他',NULL,NULL,0,0,NULL),(62,28,'发信箱',NULL,'/browseMail.action',0,0,NULL),(63,28,'写邮件',NULL,'/browseWriteMail.action',0,0,NULL),(64,28,'通讯簿',NULL,'/browseContacts.action',0,0,NULL),(65,28,'邮件设置',NULL,'/setMail.action',0,0,NULL),(66,29,'上报查询',NULL,NULL,0,0,NULL),(67,29,'上报设置',NULL,NULL,0,0,NULL),(68,29,'上报日志',NULL,NULL,0,0,NULL),(69,53,'排放统计月报-小时',NULL,NULL,0,0,NULL),(70,19,'运行维护',NULL,NULL,0,0,NULL),(71,70,'运维统计',NULL,NULL,0,0,NULL),(72,70,'运维配置',NULL,NULL,0,0,NULL),(73,71,'在线率概况',NULL,NULL,0,0,NULL),(74,71,'测点在线率详情',NULL,NULL,0,0,NULL),(75,71,'数据采样统计',NULL,NULL,0,0,NULL),(76,72,'维护组配置',NULL,NULL,0,0,NULL),(77,72,'维护类型配置',NULL,NULL,0,0,NULL),(78,72,'运行日历设置',NULL,NULL,0,0,NULL),(79,72,'仪器运行率标准',NULL,NULL,0,0,NULL),(80,20,'企业信息管理',NULL,'/browseEnt_view.action',0,0,NULL),(81,20,'排水口管理',NULL,'/browseOutfall_view.action',0,0,NULL),(82,20,'进水口管理',NULL,'/browseInlet.action',0,0,NULL),(83,20,'数采仪管理',NULL,'/browseSCY_view.action',0,0,NULL),(84,20,'进排口设置',NULL,'/navigation_html/Second/O&I_Set.html',0,0,NULL),(85,21,'在线列表',NULL,'/navigation_html/First/1-RealTimeMonitoring/OnlineTable.jsp',0,0,NULL),(86,21,'采样控制',NULL,NULL,0,0,NULL),(87,21,'报警管理',NULL,NULL,0,0,NULL),(88,21,'分析统计',NULL,NULL,0,0,NULL),(89,21,'分析统计',NULL,NULL,0,0,NULL),(90,86,'远程控制',NULL,NULL,0,0,NULL),(91,86,'采样计划',NULL,NULL,0,0,NULL),(92,86,'超标采样标准',NULL,NULL,0,0,NULL),(93,86,'采样任务查询',NULL,NULL,0,0,NULL),(94,86,'采样任务下达',NULL,NULL,0,0,NULL),(95,87,'报警日志',NULL,NULL,0,0,NULL),(96,87,'门禁管理',NULL,NULL,0,0,NULL),(97,88,'月报',NULL,NULL,0,0,NULL),(98,88,'季报',NULL,NULL,0,0,NULL),(99,88,'自定义报表',NULL,NULL,0,0,NULL),(100,88,'采样数据',NULL,NULL,0,0,NULL),(102,22,'框架管理',NULL,NULL,0,0,NULL),(103,22,'权限管理',NULL,NULL,0,0,NULL),(104,102,'模块列表','module:listModule','/systemManage/frameworkManage/moduleManage.jsp',1,0,''),(106,103,'角色列表','role:list','/systemManage/authManage/roleManage.jsp',1,0,''),(107,103,'用户列表','user:list','/systemManage/authManage/userManage.jsp',1,0,''),(108,22,'运行管理','',NULL,0,0,NULL),(109,108,'在线用户','onlineUser:list','/systemManage/runManage/onlineUser.jsp',1,0,''),(113,103,'角色增加','role:add','',1,1,''),(114,102,'导航增加','module:add','',1,1,''),(115,102,'模块删除','module:delete','',1,1,''),(116,102,'模块更新','module:update','',1,1,''),(117,102,'模块权限管理','module:listOps',NULL,1,1,NULL),(118,102,'导航更新','navigation:update','',1,1,''),(119,102,'导航管理','navigation:listRoleAuths','/systemManage/frameworkManage/navManage.jsp',1,0,'由角色名得到对应导航列表'),(120,103,'角色增加','role:add','',1,1,''),(121,103,'角色删除','role:delete','',1,1,''),(122,103,'角色更新','role:update','',1,1,''),(123,103,'用户增加','user:add','',1,1,''),(124,103,'用户删除','user:delete','',1,1,''),(125,103,'用户更新','user:update','',1,1,''),(126,103,'用户修改角色','user:modifyRoles','',1,1,''),(128,108,'下线用户','onlineUser:stop','',1,1,'');
 /*!40000 ALTER TABLE `module` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -59,12 +59,13 @@ CREATE TABLE `role` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `parent_id` int(11) NOT NULL COMMENT '创造Role的User_id',
   `role_name` varchar(50) DEFAULT NULL,
+  `ent_no` int(11) DEFAULT NULL,
   `role_cn_name` varchar(45) DEFAULT NULL COMMENT '备注',
   `comment` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_UNIQUE` (`id`),
   UNIQUE KEY `role_name_index` (`role_name`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -73,7 +74,7 @@ CREATE TABLE `role` (
 
 LOCK TABLES `role` WRITE;
 /*!40000 ALTER TABLE `role` DISABLE KEYS */;
-INSERT INTO `role` VALUES (1,0,'admin','超级管理员','test'),(2,0,'guest','陌生',''),(5,58,'role1','测试角色','测试角色');
+INSERT INTO `role` VALUES (1,0,'admin',104,'超级管理员','test'),(2,0,'guest',NULL,'陌生',''),(6,58,'pepa',NULL,'省级环保局','provience environmental protection agency'),(7,58,'bgbxg',101,'宝钢不锈钢公司','宝钢不锈钢公司人员'),(8,58,'zhgy',109,'中海工业上海长兴','中海工业上海长兴');
 /*!40000 ALTER TABLE `role` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -94,7 +95,7 @@ CREATE TABLE `role_auth` (
   KEY `role_module_idx` (`role_id`,`module_id`),
   CONSTRAINT `module_id_fk` FOREIGN KEY (`module_id`) REFERENCES `module` (`id`),
   CONSTRAINT `role_fk` FOREIGN KEY (`role_id`) REFERENCES `role` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=48 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=280 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -103,7 +104,7 @@ CREATE TABLE `role_auth` (
 
 LOCK TABLES `role_auth` WRITE;
 /*!40000 ALTER TABLE `role_auth` DISABLE KEYS */;
-INSERT INTO `role_auth` VALUES (17,1,12,0),(18,1,13,0),(19,1,14,0),(20,1,11,0),(27,2,11,0),(28,2,12,0),(29,1,110,0),(30,1,111,1),(39,1,104,0),(40,1,105,0),(41,1,106,0),(42,1,107,0),(43,1,109,0),(44,5,104,0),(45,5,106,0),(46,5,12,0),(47,5,13,0);
+INSERT INTO `role_auth` VALUES (229,1,128,0),(230,1,104,0),(231,1,106,0),(232,1,11,0),(233,1,107,0),(234,1,12,0),(235,1,109,0),(236,1,113,0),(237,1,114,0),(238,1,115,0),(239,1,116,0),(240,1,117,0),(241,1,118,0),(242,1,119,0),(243,1,120,0),(244,1,121,0),(245,1,122,0),(246,1,123,0),(247,1,124,0),(248,1,125,0),(249,1,126,0),(250,6,128,0),(251,6,104,0),(252,6,106,0),(253,6,11,0),(254,6,107,0),(255,6,12,0),(256,6,109,0),(257,6,113,0),(258,6,117,0),(259,6,120,0),(260,6,121,0),(261,6,122,0),(262,6,123,0),(263,6,124,0),(264,6,125,0),(265,6,126,0),(274,7,119,0),(275,7,107,0),(276,8,104,0),(277,8,107,0),(278,2,106,0),(279,2,109,0);
 /*!40000 ALTER TABLE `role_auth` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -124,7 +125,7 @@ CREATE TABLE `role_user` (
   KEY `user_id_fk_idx` (`user_id`),
   CONSTRAINT `role_id_fk1` FOREIGN KEY (`role_id`) REFERENCES `role` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `user_id_fk` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=70 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=82 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -133,7 +134,7 @@ CREATE TABLE `role_user` (
 
 LOCK TABLES `role_user` WRITE;
 /*!40000 ALTER TABLE `role_user` DISABLE KEYS */;
-INSERT INTO `role_user` VALUES (67,1,58),(69,5,61);
+INSERT INTO `role_user` VALUES (74,1,58),(77,6,62),(79,8,64),(80,2,65),(81,7,63);
 /*!40000 ALTER TABLE `role_user` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -161,7 +162,7 @@ CREATE TABLE `user` (
   UNIQUE KEY `id_UNIQUE` (`id`),
   UNIQUE KEY `id_name_index` (`id`,`user_name`),
   UNIQUE KEY `name_UNIQUE` (`user_name`)
-) ENGINE=InnoDB AUTO_INCREMENT=62 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=66 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -170,7 +171,7 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (58,'liqiang','b070f1b8b997320ea8b857ca835fdc70163c5a38cc7ca3ccc5942c7cb0139bddc15c29aaabba5e30c9a920195e084f2bf6ce60276eb155bacfa64416e07b42c3','2018-05-01 17:12:41','李强',1,'18340861214','','1214098151@qq.com','2018-05-01 17:12:41','e53a60412829d2a8c5c7e6f0a98bd716',1),(61,'test','45a1698c81c21c326a67b79395c760a81c59984f37239efb6c9de98a734fe0672ea778221b08acb5013b09f9c020e8d7ccbb58efbb199b05a6f480cbfb32b6a3','2018-05-01 23:08:28','test',0,'18340861214','test','1214098151@qq.com','2018-05-01 23:08:28','0d24d04d0817786295de47aa93addb0d',1);
+INSERT INTO `user` VALUES (58,'liqiang','45a1698c81c21c326a67b79395c760a81c59984f37239efb6c9de98a734fe0672ea778221b08acb5013b09f9c020e8d7ccbb58efbb199b05a6f480cbfb32b6a3','2018-05-01 17:12:41','李强',1,'18340861214','','1214098151@qq.com','2018-05-05 15:55:48','0d24d04d0817786295de47aa93addb0d',1),(62,'pepa','2d51bd93a417a90eefb4f2567774cd82b8b63182f72a835788b9b37483d76cb13789ca19f5f2310ec9ef7f2c4a7a8abe5dd364861ce4ccbe9976aa722d376578','2018-05-06 14:39:20','省环保局人员',0,'18340861214','test','1214098151@qq.com','2018-05-06 14:39:20','20f9101ec0dd0c5c668540da46e3da7c',1),(63,'bgbxg','fc5a3f3d9248e7bfad22a750cfb4bba7868a5bcc60669e35e7f158920952d55a7a576c6f41891ffba795017587b4dea97957ab2324de076d3867255ad28c56b2','2018-05-06 15:31:31','宝钢不锈钢公司',1,'18340861214','','1214098151@qq.com','2018-05-06 17:34:48','d49dc9a00f213853f0c90c220723d8cf',2),(64,'zhgy','1b4c197fefe78ca615079d3f0a1904efe4dc62d096261e0eeb402b9c20ddc481654d934b3f73b468ca794bf8f89efd04c51fb25b59fce5bb7ba2040e3fe4e66f','2018-05-06 15:43:43','中海工业上海长兴',1,'18340861214','','1214098151@qq.com','2018-05-06 15:43:43','8e84f1409659e8aeb5e3fdd48f49b154',1),(65,'guest','dab286753ae1243927edfed77020e76cdc33836bc5e45a2cbc1fc33eac0b731bd88e78f62dd15fa3a2de1c5d7a45ed2d0b29e785b079956796dcfd2f6bd6acc6','2018-05-06 17:29:08','guest',0,'18340861214','','1214098151@qq.com','2018-05-06 17:29:08','c8693a304c415890c8fc211b23a574c3',1);
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -183,4 +184,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-05-01 23:46:18
+-- Dump completed on 2018-05-06 17:42:34
