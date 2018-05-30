6ebdeaa1a9a45ad4d3ef1766c6421ed1dd94f003
QiangL <iamlq@live.cn>
2018-05-30 20:11:46 +0800

    update generateConfig.xml

--- src/test/java/com/test/SynchronizedBloceDemo.java
+++ src/test/java/com/test/SynchronizedBloceDemo.java
@@ -1,16 +1,32 @@
 package com.test;
 
+import com.sun.xml.internal.messaging.saaj.util.ByteOutputStream;
+import org.apache.shiro.codec.Base64;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.crypto.Cipher;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.io.StringReader;
+import java.lang.ref.Reference;
+import java.lang.ref.ReferenceQueue;
+import java.lang.ref.WeakReference;
+import java.util.Arrays;
 import java.util.TreeMap;
 
 public class SynchronizedBloceDemo {
 
-    private static final Logger logger= LoggerFactory.getLogger(SynchronizedBloceDemo.class);
     public static void main(String[] args) throws Exception {
+        Cipher cipher = Cipher.getInstance("AES");
+        byte[] key = Base64.decode("4AvVhmFLUs0KTA3Kprsdag==");
 
-        logger.info("asdfasdf ");
+
+        System.out.println(Arrays.toString(key));
+
+        System.out.println(key.length);
+
     }
 
 }
