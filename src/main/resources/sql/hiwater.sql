08a4a835e5d2429a859b1a202ed99afa065a270d
QiangL <iamlq@live.cn>
2018-07-09 23:17:57 +0800

    更新sql

--- src/main/resources/sql/hiwater.sql
+++ src/main/resources/sql/hiwater.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `hiwater` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `hiwater`;
 -- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
 --
 -- Host: localhost    Database: hiwater
@@ -183,4 +185,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-06-14 11:32:03
+-- Dump completed on 2018-07-09 23:17:16
